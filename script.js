3ed2bb4931f782d1624ca830592775c193137fbd
Rene Balog-Dutombe <balogdrene@gmail.com>
2022-05-09 15:03:58 +0200

    change grid button working

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const container = document.getElementById("container");
+const gridcontainer = document.getElementById("gridcontainer");
+const gridButton = document.getElementById("gridbutton");
+
+gridButton.addEventListener('click', () => {
+    changeSize();
+});
+
+function changeSize() {
+    reset()
+    let newGrid = prompt("New grid size")
+    makeRows(newGrid, newGrid)
+}
 
 function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+    gridcontainer.style.setProperty('--grid-rows', rows);
+    gridcontainer.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);
-        container.appendChild(cell).className = "grid-item";
+        // cell.innerText = (c + 1);
+        gridcontainer.appendChild(cell).className = "grid-item";
     };
 };
+
+function reset() {
+    document
+        .querySelectorAll(".grid-item")
+        .forEach((e) => e.parentNode.removeChild(e));
+}
+
 
 makeRows(16, 16);
